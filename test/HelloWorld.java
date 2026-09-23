73bacd3f8be4218e33640ab0247315d51d069f0f
marthasofia126 <marthasofia12@icloud.com>
2026-09-23 16:22:04 -0600

    add scanner to test file

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What's your name? ");
+        String name = in.NextLine();
+
+        System.out.println("Hello, " + name);
     }
 }
